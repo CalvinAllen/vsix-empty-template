f6b265baf2b91d09ae27a8a85056841ee68e757c
Calvin Allen <calvinallen@newrelic.com>
2023-03-27 15:37:56 -0400

    Initial commit

---
+++ src/EmptyVsix/EmptyVsixPackage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Microsoft.VisualStudio.Shell;
+using Task = System.Threading.Tasks.Task;
+
+namespace EmptyVsix
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [Guid(PackageGuid)]
+    public sealed class EmptyVsixPackage : AsyncPackage
+    {
+        private const string PackageGuid = "577CC59D-234E-4C64-AB53-C7A62CECCB74";
+
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+        }
+    }
+}
